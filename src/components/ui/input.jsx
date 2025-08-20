d9593d3ee20fb045dd103c242b8280b42e09b910
Wattky Technology <wattky@pm.me>
2025-08-20 18:35:35 +0100

    Delete src/components/ui directory

--- src/components/ui/input.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-export function Input(props){
-  const { className='', ...rest } = props
-  return <input className={`w-full px-3 py-2 border rounded-xl focus:outline-none focus:ring ${className}`} {...rest} />
-}
--- src/components/ui/slider.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-export function Slider({value=[0], max=100, step=1, onValueChange}){
-  return <input type="range" value={value[0]} max={max} step={step} onChange={e=>onValueChange?.([Number(e.target.value)])} />
-}
