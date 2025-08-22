94d8bb1a5057f1af8efed228659374116b9b23e9
Wattky Technology <wattky@pm.me>
2025-08-22 12:34:22 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
-  plugins: [react()]
-})
-({
-  plugins: [react()]
+  plugins: [react()],
 })
