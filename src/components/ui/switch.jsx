32bcbb63f0d0f864801c78358dc732929a92862e
Wattky Technology <wattky@pm.me>
2025-08-22 09:19:44 +0100

    Delete src/components/ui/switch.jsx

--- src/components/ui/switch.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-export function Switch({checked, onCheckedChange}){
-  return <input type="checkbox" role="switch" checked={!!checked} onChange={e=>onCheckedChange?.(e.target.checked)} />
-}
