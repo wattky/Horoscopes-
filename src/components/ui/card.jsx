b255d64b9bce9c639750cef9bc6989f5ee5eecbd
Wattky Technology <wattky@pm.me>
2025-08-22 09:18:59 +0100

    Delete src/components/ui/card.jsx

--- src/components/ui/card.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-export function Card({className='', ...props}){ return <div className={`bg-white dark:bg-zinc-900 border rounded-2xl ${className}`} {...props}/> }
-export function CardHeader({className='', ...props}){ return <div className={`p-4 ${className}`} {...props}/> }
-export function CardContent({className='', ...props}){ return <div className={`p-4 pt-0 ${className}`} {...props}/> }
-export function CardTitle({className='', ...props}){ return <h3 className={`text-lg font-semibold ${className}`} {...props}/> }
