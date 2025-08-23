dc651bead7434334344d9caa5de3e258e5a7090a
Wattky Technology <wattky@pm.me>
2025-08-23 09:31:17 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-// main.jsx
 import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'   // 👈 this ensures Tailwind CSS is included
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 )
