b3a62e69e58f4e656c9bc87358cee32381c22aac
Wattky Technology <wattky@pm.me>
2025-08-24 16:17:15 +0100

    Delete supabase/supabase.js

--- supabase/supabase.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-// Correct: use the variable names you actually set in Netlify
-const supabaseUrl = import.meta.env.VITE_SUPABASE_DATABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
