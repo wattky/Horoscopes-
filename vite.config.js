467a609f28426e28537503771d5fe6a8e02ea98b
Wattky Technology <wattky@pm.me>
2025-08-25 13:21:56 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // 👈 ensures assets load correctly on Netlify
-  build: {
-    outDir: 'dist',
-  },
+  base: '/', // 👈 this ensures assets are linked from root
 })
