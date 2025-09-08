9cadeeade7e62f6883ea9c84283648757c3621dc
Wattky Technology <wattky@pm.me>
2025-09-08 10:18:43 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Horoscopes-/', // 👈 must match your repo name
+  base: '/', // 👈 must match your repo name
 })
